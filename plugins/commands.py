7d4b961a008a2288482f9f2b622d411ddacc8d82
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-16 23:33:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,13 +87,12 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕️ Add Me To Your Group ➕️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🤠 Dev 🤠', url='https://t.me/P_JUPITER'), 
-            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/cinemapranthanzz1')
-            ],[      
-            InlineKeyboardButton('ℹ️ Help ℹ️', callback_data='help'),
-            InlineKeyboardButton('😊 About 😊', callback_data='about')
+            InlineKeyboardButton('⚜️ 𝕆𝕌ℝ ℂℍ𝔸ℕℕ𝔼𝕃 ⚜️', url='https://t.me/cinemapranthanzz5')
+            ],[                       
+            InlineKeyboardButton('⚡️ 𝕄𝔸𝕀ℕ 𝔾ℝ𝕆𝕌ℙ ⚡️', url='https://t.me/cinemapranthanzz1')
+            ],[ 
+            InlineKeyboardButton('🔰 𝔸𝔹𝕆𝕌𝕋 🔰', callback_data='about'), 
+            InlineKeyboardButton('ℹ️ ℍ𝔼𝕃ℙ ℹ️', callback_data='help')            
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action("Typing")
