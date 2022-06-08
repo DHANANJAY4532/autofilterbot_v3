9f0b6b04efae2eadc324acee54bf76ca34726498
its_me_jupiter <102791126+DHANANJAY4532@users.noreply.github.com>
2022-06-08 15:39:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -773,13 +773,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)       
         await query.message.edit_text(
-            text="➤"
+            text="███▒▒▒▒▒▒▒"
         )
         await query.message.edit_text(
-            text="➤➤➤"
+            text="███████▒▒▒
+"
         )
         await query.message.edit_text(
-            text="➤➤➤➤➤➤➤"
+            text="██████████"
         )
         await query.message.edit_text(                     
             text=script.HELP_TXT.format(query.from_user.mention),
@@ -801,13 +802,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text="➤"
+            text="███▒▒▒▒▒▒▒"
         )
         await query.message.edit_text(
-            text="➤➤➤"
+            text="███████▒▒▒"
         )
         await query.message.edit_text(
-            text="➤➤➤➤➤➤➤"
+            text="██████████"
         )
         await query.message.edit_text(
             text=script.ABOUT_TXT,        
