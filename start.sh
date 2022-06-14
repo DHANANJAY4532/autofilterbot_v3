4fb0349db5d9b58f6d1ec5fe9e7d9c653732a6a8
its_me_jupiter <102791126+DHANANJAY4532@users.noreply.github.com>
2022-06-14 19:19:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DHANANJAY4532/PROFESSOR-BOT.git /PROFESSOR-BOT
+  git clone https://github.com/DHANANJAY4532/autofilterbot_v3 .git /autofilterbot_v3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PROFESSOR-BOT
+  git clone $UPSTREAM_REPO /autofilterbot_v3
 fi
-cd /PROFESSOR-BOT
+cd /autofilterbot_v3
 pip3 install -U -r requirements.txt
 echo "STARTING.......🔥"
 python3 bot.py
