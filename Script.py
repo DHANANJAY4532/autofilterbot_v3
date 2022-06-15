bcd827bc984e5e94cbe16c7fbe0170f1f7bf6cdc
its_me_jupiter <102791126+DHANANJAY4532@users.noreply.github.com>
2022-06-15 19:41:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """👋 𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>,\n im manager of malayali freaks 🙂"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>,\n  IM MANAGER OF "MALAYALI FREAKS" 🙂"""
     HELP_TXT = """𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """ㅤ """
     SOURCE_TXT = """<b>NOKKI IRUNNO IPPO KITTUM"""
